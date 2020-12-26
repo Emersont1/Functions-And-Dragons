b5ef7e589776bd29e57f5e115f82ec7061423c52
Peter Taylor <info@emersont1.co.uk>
2020-12-26 13:23:14 +0000

    Some more features, and added internals to a separate file

--- src/Dice.hs
+++ src/Dice.hs
@@ -1,24 +1,30 @@
 module Dice where
 import Roll
+import RollInternal
 import Data.Ratio
 import Data.Bifunctor
--- data Roll a = Value a Rational
-type Dice = Rolls Integer
 
+
+type Dice = Rolls Integer
 
 d :: Integer -> Integer -> Dice
 d 1 f = RollCollection $map (\x-> Value x ((%) 1 f)) [1..f] 
 d n f 
-    | even n = simplify $ mix (+) ((n `div` 2)`d` f) ((n `div` 2)`d` f)
-    | odd n  = simplify $ mix (+) ((n-1)`d` f) (1 `d` f)
+    | even n = simplify $ mixFmap (+) ((n `div` 2)`d` f) ((n `div` 2)`d` f)
+    | odd n  = simplify $ mixFmap (+) ((n-1)`d` f) (1 `d` f)
 
 p :: Dice -> Integer -> Dice
-p d n = simplify $ fmap (\x-> max (n + x) 0) d
+p d n = simplify $ fmap (\x-> Prelude.max (n + x) 0) d
 
 -- Same as above, just permits negative values
 pNeg :: Dice -> Integer -> Dice
 pNeg d n = simplify $ fmap (+n) d
 
-_combs :: [Dice] -> [([Integer], [Rational])]
-_combs ((RollCollection x):xs) = concatMap (\(Value u v) -> map ( bimap (u:) (v:) )(_combs xs)) x
+empty :: a -> [a]
+empty x = tail [x]
+
+
+_combs :: [Dice] -> Rolls [Integer]
+_combs [] = RollCollection [Value (empty 1) ((%)1 1)]
+_combs (x:xs) = mixFmap (:) x $_combs xs 
 
--- src/Roll.hs
+++ src/Roll.hs
@@ -1,30 +1,43 @@
-module Roll where
+module Roll (Rolls (RollCollection), uniqueValues, probability, maxValue, minValue, simplify, mix, mixFmap, fmapSimplify) where
 
+import RollInternal
 import Data.List
-
-data Roll a = Value a Rational
-
-_prob :: Roll a -> Rational
-_prob (Value _ x) = x
-
-_val :: Roll a -> a
-_val (Value x _) = x
 
 newtype Rolls a = RollCollection [Roll a]
 
-instance Functor Rolls  where
-    fmap f (RollCollection xs) = RollCollection $ map (\(Value x y) -> Value (f x) y) xs
+instance Functor Rolls where
+  fmap f (RollCollection xs) = RollCollection $ map (\(Value x y) -> Value (f x) y) xs
+
+instance Show a => Show (Rolls a) where
+    show (RollCollection xs) = "(" ++ intercalate ", " (map show  xs)++ ")"
 
 uniqueValues :: (Eq a) => Rolls a -> [a]
 uniqueValues (RollCollection rs) = nub $ map _val rs
 
-probability ::(Eq a) => Rolls a -> a -> Rational 
+probability :: (Eq a) => Rolls a -> a -> Rational
 probability (RollCollection rs) v = sum $ map _prob $ filter (\x -> v == _val x) rs
 
-simplify :: (Eq a) => Rolls a -> Rolls a
-simplify rs = RollCollection $ map (\r ->
-     Value r $ probability  rs r)
-     $ uniqueValues rs
+maxValue :: (Ord a) => Rolls a -> a
+maxValue (RollCollection rs) = maximum $ map _val rs
 
-mix :: (a-> b-> c) -> Rolls a -> Rolls b -> Rolls c
-mix f (RollCollection xs) (RollCollection ys) = RollCollection [Value (f xv yv) $xp*yp | (Value xv xp)<- xs, (Value yv yp)<-ys]
+minValue :: (Ord a) => Rolls a -> a
+minValue (RollCollection rs) = minimum $ map _val rs
+
+simplify :: (Eq a) => Rolls a -> Rolls a
+simplify rs =
+  RollCollection $
+    map
+      ( \r ->
+          Value r $ probability rs r
+      )
+      $ uniqueValues rs
+
+mix :: Rolls a -> Rolls b -> Rolls (a, b)
+mix (RollCollection xs) (RollCollection ys) = RollCollection [Value (xv, yv) $xp * yp | (Value xv xp) <- xs, (Value yv yp) <- ys]
+
+mixFmap :: (a -> b -> c) -> Rolls a -> Rolls b -> Rolls c
+mixFmap f xs ys = fmap (uncurry f) (mix xs ys)
+
+fmapSimplify :: Eq b => (a -> b) -> Rolls a -> Rolls b
+fmapSimplify f xs = simplify $ fmap f xs
+
